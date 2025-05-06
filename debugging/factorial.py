3ec06baa5111620c6b78727d988240791efe9761
OkanMahmudov <mahmudovokan47@gmail.com>
2025-05-06 17:32:54 +0400

    chmod + readme

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -1,21 +1,18 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
 import sys
 
 def factorial(n):
     result = 1
     while n > 1:
         result *= n
-        n = n - 1
+        n -= 1
     return result
 
-if len(sys.argv) != 2:
-    print("Usage: python3 script.py <number>")
+if len(sys.argv) < 2:
+    print("Usage: ./factorial.py <number>")
     sys.exit(1)
 
-try:
-    num = int(sys.argv[1])
-    f = factorial(num)
-    print(f)
-except ValueError:
-    print("Please provide a valid integer argument.")
-    sys.exit(1)
+f = factorial(int(sys.argv[1]))
+print(f)
